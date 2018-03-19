2d7527881b4648c25b4e6e502188253518efeff9
artusvranken <artusvranken@gmail.com>
2018-03-19 09:12:33 +0100

    Added some files.

---
+++ js/FarmToFork.js
@@ -0,0 +1,22 @@
+// Require dependencies.
+const driver = require('bigchain-driver');
+const bip39 = require('bip39');
+
+// Require the dotenv library.
+require('dotenv').config()
+
+export class FieldToFork {
+
+    /**
+     * Initialise a new class that we'll use to handle our connection with the network.
+     */
+    constructor() {
+        // Initialise a new connection.
+        this.connection = new driver.Connection(process.env.APP_URL, {
+            app_id: process.env.APP_ID,
+            app_key: process.env.APP_KEY
+        });
+    }
+
+    // Here we'll write our methods.
+}
