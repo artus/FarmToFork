2fde1584b16c93c7a2798da8688886444aaa226d
artusvranken <artusvranken@gmail.com>
2018-03-20 17:24:30 +0100

    Actions can now be performed on food-items.

--- js/FarmToFork.js
+++ js/FarmToFork.js
@@ -15,7 +15,7 @@ class FarmToFork {
         // Initialise a new connection.
         this.connection = new driver.Connection(process.env.APP_URL, {
             app_id: process.env.APP_ID,
-            app_key: process.env.APP_KEY
+            app_key: process.env.APP_KEY,
         });
 
         this.currentIdentity = this.generateKeypair("ftf");
@@ -26,10 +26,146 @@ class FarmToFork {
     /**
      * Generate a keypair based on the supplied seed string.
      * @param {string} keySeed - The seed that should be used to generate the keypair.
+     * @returns {*} The generated keypair.
      */
     generateKeypair(keySeed) {
         if (typeof keySeed == "undefined" || keySeed == "") return new driver.Ed25519Keypair();
         return new driver.Ed25519Keypair(bip39.mnemonicToSeed(keySeed).slice(0, 32));
+    }
+
+    createAsset(foodItem) {
+
+        return new Promise((resolve, reject) => {
+
+            // Create asset object.
+            const assetData = {
+                "type": "FtfTutorialAsset",
+                "item": foodItem,
+            };
+
+            // Create metadata object.
+            const metaData = {
+                "action": "Introduced",
+                "date": new Date().toISOString(),
+            };
+
+            // Create a CREATE transaction.
+            const introduceFoodItemToMarketTransaction = driver.Transaction.makeCreateTransaction(
+
+                // Include the foodItem as asset data.
+                assetData,
+                // Include metadata to give information on the action.
+                metaData,
+                // Create an output.
+                [driver.Transaction.makeOutput(
+                    driver.Transaction.makeEd25519Condition(this.currentIdentity.publicKey))],
+                // Include the public key
+                this.currentIdentity.publicKey
+            );
+
+            // We sign the transaction
+            const signedTransaction = driver.Transaction.signTransaction(introduceFoodItemToMarketTransaction, this.currentIdentity.privateKey);
+
+            // Post the transaction to the network
+            this.connection.postTransaction(signedTransaction).then(response => {
+
+                // Check the status and return when succesfull
+                return this.connection.pollStatusAndFetchTransaction(response.id);
+            }).then(postedTransaction => {
+
+                // Let the promise resolve the created transaction.
+                resolve(postedTransaction);
+
+                // Catch exceptions
+            }).catch(err => {
+
+                reject(err);
+            })
+        });
+
+    }
+
+    /**
+     * Get a list of ids of unspent transactions for a certain public key.
+     * @returns {Array} An array containing all unspent transactions for a certain public key.
+     */
+    getAssets() {
+
+        return new Promise((resolve, reject) => {
+
+            // Get a list of ids of unspent transactions from a public key.
+            this.connection.listOutputs(this.currentIdentity.publicKey, false).then(response => {
+
+                resolve(response);
+            });
+        }).catch(err => {
+            reject(err);
+        })
+
+    }
+
+    /**
+     * Load a transaction by using its transaction id.
+     * @param {*} transactionId 
+     */
+    loadTransaction(transactionId) {
+        return new Promise((resolve, reject) => {
+
+            // Get the transaction by its ID.
+            this.connection.getTransaction(transactionId).then(response => {
+                resolve(response);
+            }).catch(err => {
+                reject(err);
+            })
+        });
+    }
+
+    updateAsset(transaction, action) {
+
+        return new Promise((resolve, reject) => {
+
+            console.log(transaction);
+
+            // Create metadata for action.
+            const metaData = {
+                "action": action,
+                "date": new Date().toISOString()
+            };
+
+            // Create a new TRANSFER transaction.
+            const updateAssetTransaction = driver.Transaction.makeTransferTransaction(
+
+                // previous transaction.
+                [{ tx: transaction, output_index: 0 }],
+
+                // Create new output.
+                [driver.Transaction.makeOutput(driver.Transaction.makeEd25519Condition(this.currentIdentity.publicKey))],
+
+                // Add metadata.
+                metaData
+            )
+
+            // Sign new transaction.
+            const signedTransaction = driver.Transaction.signTransaction(updateAssetTransaction, this.currentIdentity.privateKey);
+
+            console.log("Posting transaction.");
+            // Post the new transaction.
+            this.connection.postTransaction(signedTransaction).then(response => {
+
+                console.log("Transaction posted.");
+
+                // Poll for status.
+                return this.connection.pollStatusAndFetchTransaction(response.id);
+            }).then(postedTransaction => {
+
+                // Return the posted transaction to the callback function.
+                resolve(postedTransaction);
+
+            }).catch(err => {
+                reject(err);
+            });
+        });
+
     }
 }
 
--- js/ftf-web.js
+++ js/ftf-web.js
@@ -5,7 +5,21 @@ var ftfApp = new Vue({
         activePane: 'identity',
 
         // Inputs
-        identitySeedInput: "",
+        identitySeedInput: "ftf",
+        assetInput: "",
+        actionInput: "",
+        activeTransaction: {
+            "asset": {
+                "data": {
+                    "item": "Loading..."
+                }
+            }
+        },
+
+        // Assets
+        transactionIds: new Array(),
+        assets: new Array(),
+        transactionsForAsset: new Array(),
     },
     methods: {
         setActive(pane) {
@@ -14,9 +28,84 @@ var ftfApp = new Vue({
         isActive(pane) {
             return this.activePane == pane;
         },
+        getAssets() {
+            return this.assets;
+        },
+
+        // Forms
         identityButtonClicked() {
             this.farmToFork.currentIdentity = this.farmToFork.generateKeypair(this.identitySeedInput);
+        },
+        assetButtonClicked() {
+            if (this.assetInput == "") return;
+            this.farmToFork.createAsset(this.assetInput).then(response => {
+
+                console.log("New asset added.");
+                // Do nothing, just reload the asset list.
+                ftfApp.loadAssetsIds();
+            });
+        },
+
+        // Menu
+        menuClicked(link) {
+
+            switch (link) {
+                case "identity":
+                    this.activePane = "identity";
+                    break;
+
+                case "assets":
+                    this.loadAssetsIds();
+                    this.activePane = "assets";
+                    break;
+            }
+        },
+
+        // Loading assets
+        loadAssetsIds() {
+
+            this.farmToFork.getAssets().then(response => {
+                ftfApp.transactionIds = response;
+                ftfApp.loadAssetsFromTransactionIds();
+            });
+
+        },
+        loadAssetsFromTransactionIds() {
+
+            this.assets = new Array();
+
+            for (let transaction of this.transactionIds) {
+                this.farmToFork.connection.getTransaction(transaction.transaction_id).then(response => {
+                    if (response.operation == 'CREATE') return ftfApp.farmToFork.connection.listTransactions(response.id, 'CREATE');
+                    return ftfApp.farmToFork.connection.listTransactions(response.asset.id, 'CREATE');
+                }).then(responseCreate => {
+                    ftfApp.assets.push(responseCreate[0]);
+                }).catch(err => {
+                    console.log(transaction.transaction_id);
+                })
+            }
+        },
+        transactionClicked(id) {
+            this.farmToFork.connection.getTransaction(id).then(response => ftfApp.activeTransaction = response);
+            this.loadTransactionsForAsset(id);
+            this.setActive('transactions');
+        },
+        loadTransactionsForAsset(assetId) {
+
+            this.farmToFork.connection.listTransactions(assetId).then(response => ftfApp.transactionsForAsset = response);
+        },
+        actionButtonClicked() {
+            console.log(this.activeTransaction);
+            this.farmToFork.connection.listTransactions(this.activeTransaction.id).then(response => {
+                return ftfApp.farmToFork.updateAsset(response[response.length - 1], ftfApp.actionInput);
+            }).then(response => {
+                ftfApp.loadTransactionsForAsset(ftfApp.activeTransaction.id);
+            });
+            /*this.farmToFork.updateAsset(this.activeTransaction, this.actionInput).then(response => {
+                ftfApp.loadTransactionsForAsset(ftfApp.activeTransaction.id);
+            })*/
         }
+
 
     }
 });
